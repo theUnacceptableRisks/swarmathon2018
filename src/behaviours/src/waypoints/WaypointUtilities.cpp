9e2f8a66e876f126e14374cabf4830fb1ac7af61
Daniel R. Koris <korisd@gmail.com>
2017-12-30 18:27:17 -0500

    renamed waypointutility

---
+++ src/behaviours/src/waypoints/WaypointUtilities.cpp
@@ -0,0 +1,12 @@
+#include "WaypointUtilities.h"
+
+
+float WaypointUtilities::getDistance( DrivingParams params )
+{
+    return 0.0;
+}
+
+float WaypointUtilities::getAngularCorrectionNeeded( DrivingParams params )
+{
+    return 0.0;
+}
---
+++ src/behaviours/src/waypoints/WaypointUtilities.h
@@ -0,0 +1,23 @@
+#ifndef waypointutility_h
+#define waypointutility_h
+
+#include <math.h>
+#include <angles/angles.h>
+
+namespace WaypointUtilities
+{
+    typedef struct standard_driving_params
+    {
+        const float goal_x;
+        const float goal_y;
+        const float current_x;
+        const float current_y;
+        const float current_theta;
+    } DrivingParams;
+
+    float getDistance( DrivingParams params );
+    float getAngularCorrectionNeeded( DrivingParams params );
+
+};
+
+#endif
