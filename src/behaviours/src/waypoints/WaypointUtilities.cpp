440c640468d145080a53bf1d0846ffd2e602f3c5
Daniel R. Koris <korisd@gmail.com>
2017-12-30 18:52:32 -0500

    got two basic but important functions into the utilities lib

--- src/behaviours/src/waypoints/WaypointUtilities.cpp
+++ src/behaviours/src/waypoints/WaypointUtilities.cpp
@@ -3,10 +3,18 @@
 
 float WaypointUtilities::getDistance( DrivingParams params )
 {
-    return 0.0;
+    //simple hypotenuse of the difference between the two points
+    //order shouldnt matter as long as its consistant
+    return hypot( fabs( params.goal_x - params.current_x ),
+                  fabs( params.goal_y - params.current_y ) );
 }
 
 float WaypointUtilities::getAngularCorrectionNeeded( DrivingParams params )
 {
-    return 0.0;
+    //goal_theta -> tan = opposite/adjacent, ie this gives us an angular heading we need to be on to reach out destination
+    //shortest_angular_distance finds the smaller of the two amounts we could rotate in either direction to arrive there
+
+    float goal_theta = atan2( ( params.goal_y - params.current_y ), ( params.goal_x - params.current_x ) );
+
+    return angles::shortest_angular_distance( params.current_theta, goal_theta );
 }
