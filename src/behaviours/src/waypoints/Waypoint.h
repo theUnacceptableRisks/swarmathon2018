22b22fe1fe9ed387ea51b70801ae68baa81a1e69
Daniel R. Koris <korisd@gmail.com>
2018-01-06 12:35:15 -0500

    added has arrived features to waypoints

--- src/behaviours/src/waypoints/Waypoint.h
+++ src/behaviours/src/waypoints/Waypoint.h
@@ -6,15 +6,17 @@
 class Waypoint : public StateMachine
 {
     public:
-        Waypoint()
+        Waypoint() : has_arrived(false)
         {
             outputs.addElement( "left_velocity", new IOInt( &output_left_velocity ) );
             outputs.addElement( "right_velocity", new IOInt( &output_right_velocity ) );
         }
+        bool hasArrived( void ) { return has_arrived; }
     protected:
         void setOutputLeftVelocity( int vel ) { output_left_velocity = vel; }
         void setOutputRightVelocity( int vel ) { output_right_velocity = vel; }
     private:
+        bool has_arrived;
         int output_left_velocity;
         int output_right_velocity;
 };
