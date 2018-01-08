4470219f225326cafa88af5193c4a0f66794eda0
Daniel R. Koris <korisd@gmail.com>
2018-01-08 16:39:08 -0500

    working on the .h file for the new logic machine

--- src/behaviours/src/logic/LogicMachine.h
+++ src/behaviours/src/logic/LogicMachine.h
@@ -1,12 +1,37 @@
 #include "../state_machine/StateMachine.h"
 #include "../waypoints/Waypoint.h"
+#include <geometry_msgs/Pose2D.h>
+#include "../Tag.h"
+#include <vector>
+
+typedef struct logic_inputs
+{
+    geometry_msgs::Pose2D	raw_odom;
+    geometry_msgs::Pose2D	odom_accel;
+    geometry_msgs::Pose2D	odom_accel_gps;
+    vector<Tag> 		tags;
+    double			us_left = 0.0;
+    double			us_right = 0.0;
+    double			us_center = 0.0;
+    float			linear_vel_odom_accel = 0.0;
+    float			linear_vel_odom_accel_gps = 0.0;
+    float			angular_vel_odom_accel = 0.0;
+    float			angular_vel_odom_accel_gps = 0.0;
+} LogicInputs;
 
 class LogicMachine : public StateMachine
 {
+    /* need to write these  inits */
+    friend class StateInit;
     friend class StateSearch;
+    friend class StatePickUp;
+    friend class StateFindHome;
+    friend class StateDropOff;
+    friend class StateObstacleAvoidance;
     public:
-        LogicMachine() : current_waypoint(0) {}
+        LogicMachine( LogicInputs *i ) : inputs(i), current_waypoint(0) {}
         Waypoint *getCurrentWaypoint() { return current_waypoint; }
     private:
+        LogicInputs *inputs;
         Waypoint *current_waypoint;
 };
