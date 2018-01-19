51ea3b2248779d7892a83c446d976ffb5de13f2a
Daniel R. Koris <korisd@gmail.com>
2018-01-19 09:27:17 -0500

    added the gripper control stuff

---
+++ src/behaviours/src/Gripper.cpp
@@ -0,0 +1,39 @@
+#include "Gripper.h"
+
+std::tuple<double,double> Gripper::getWristFingerValuesForPosition( gripper_position pos )
+{
+    double wrist = 0.0;
+    double fingers = 0.0;
+
+    switch( pos )
+    {
+        default:
+            messaging::errorMsg( __func__, "invalid gripper position passed." );
+            break;
+        case GRIPPER_UP_OPEN:
+            wrist = 0.0;
+            fingers = M_PI_2;
+            break;
+        case GRIPPER_UP_CLOSED:
+            wrist = 0.0;
+            fingers = 0.0;
+            break;
+        case GRIPPER_DOWN_OPEN:
+            wrist = 1.25;
+            fingers = M_PI_2;
+            break;
+        case GRIPPER_DOWN_CLOSED:
+            wrist = 1.25;
+            fingers = 0.0;
+            break;
+        case GRIPPER_HOVER_OPEN:
+            wrist = 0.8;
+            fingers = M_PI_2;
+            break;
+        case GRIPPER_HOVER_CLOSED:
+            wrist = 0.8;
+            fingers = 0.0;
+            break;
+    }
+    return std::make_tuple ( wrist, fingers );
+}
---
+++ src/behaviours/src/Gripper.h
@@ -0,0 +1,19 @@
+#ifndef gripper_h
+#define gripper_h
+
+#include <math.h>
+#include <tuple>
+#include "state_machine/Error.h"
+
+namespace Gripper
+{
+    typedef enum
+    {
+        GRIPPER_UP_OPEN, GRIPPER_UP_CLOSED,
+        GRIPPER_DOWN_OPEN, GRIPPER_DOWN_CLOSED,
+        GRIPPER_HOVER_OPEN, GRIPPER_HOVER_CLOSED
+    } gripper_position;
+
+    std::tuple<double,double> getWristFingerValuesForPosition( gripper_position pos );
+}
+#endif
