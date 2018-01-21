bccf3d719946d6ce1efa568502f8ce69d4241223
Daniel R. Koris <korisd@gmail.com>
2018-01-21 14:35:53 -0500

    updated logic machine for the new waypoints

--- src/behaviours/src/logic/LogicMachine.h
+++ src/behaviours/src/logic/LogicMachine.h
@@ -1,28 +1,10 @@
 #ifndef logicmachine_h
 #define logicmachine_h
 
-#include <vector>
-#include <geometry_msgs/Pose2D.h>
-
 #include "../state_machine/StateMachine.h"
 #include "../waypoints/Waypoint.h"
 #include "../Gripper.h"
-#include "../Tag.h"
-
-typedef struct logic_inputs
-{
-    geometry_msgs::Pose2D	raw_odom;
-    geometry_msgs::Pose2D	odom_accel;
-    geometry_msgs::Pose2D	odom_accel_gps;
-    std::vector<Tag> 		tags;
-    double			us_left = 0.0;
-    double			us_right = 0.0;
-    double			us_center = 0.0;
-    double			linear_vel_odom_accel = 0.0;
-    double			linear_vel_odom_accel_gps = 0.0;
-    double			angular_vel_odom_accel = 0.0;
-    double			angular_vel_odom_accel_gps = 0.0;
-} LogicInputs;
+#include "LogicTypes.h"
 
 class LogicMachine : public StateMachine
 {
--- src/behaviours/src/logic/search/SearchInit.h
+++ src/behaviours/src/logic/search/SearchInit.h
@@ -12,21 +12,11 @@ class SearchInit : public SearchStateBase
         {
             if( ssm_owner )
             {
-                WaypointUtilities::DrivingParams params;
-                SimpleWaypointSupplementalInputs s_params;
-
+                SimpleParams params;
                 params.goal_x = -2.0;
-                params.goal_y = 0;
-                params.current_x = &ssm_owner->inputs->odom_accel.x;
-                params.current_y = &ssm_owner->inputs->odom_accel.y;
-                params.current_theta = &ssm_owner->inputs->odom_accel.theta;
-                params.current_linear_vel = &ssm_owner->inputs->linear_vel_odom_accel;
-                params.current_angular_vel = &ssm_owner->inputs->angular_vel_odom_accel;
+                params.goal_y = 0.0;
 
-                s_params.odom_x = &ssm_owner->inputs->raw_odom.x;
-                s_params.odom_y = &ssm_owner->inputs->raw_odom.y;
-
-                SimpleWaypoint *waypoint = new SimpleWaypoint( params, s_params );
+                SimpleWaypoint *waypoint = new SimpleWaypoint( ssm_owner->inputs, params );
                 ssm_owner->waypoints.push_back( (Waypoint *)waypoint );
 
                 setup_complete = true;
