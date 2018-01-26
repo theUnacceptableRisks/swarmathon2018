5b0d916d72b354b27f00cdfefcba5f382ccfd85e
Daniel R. Koris <korisd@gmail.com>
2018-01-26 14:46:57 -0500

    reverted the pid configs

--- src/behaviours/src/waypoints/SimpleWaypoint.cpp
+++ src/behaviours/src/waypoints/SimpleWaypoint.cpp
@@ -32,24 +32,24 @@ SimpleWaypoint::SimpleWaypoint( LogicInputs *i, SimpleParams sp ) : Waypoint( i 
 
     PIDConfig vel_config;
     vel_config.Kp = 60; //proportional constant
-    vel_config.Ki = 0; //integral constant
-    vel_config.Kd = 0; //derivative constant
+    vel_config.Ki = 10; //integral constant
+    vel_config.Kd = 2; //derivative constant
     vel_config.satUpper = 255; //upper limit for PID output
     vel_config.satLower = -255; //lower limit for PID output
     vel_config.antiWindup = vel_config.satUpper; //prevent integral from acruing error untill proportional out$
     vel_config.errorHistLength = 4; //how many time steps to average error over
     vel_config.alwaysIntegral = true; //should the integral alway be on or only when there is error
     vel_config.resetOnSetpoint = true; //reset the integral and error history whent he setpoint changes
-    vel_config.feedForwardMultiplier = 0; //gives 127 pwm at 0.4 commandedspeed  ORIG:320
+    vel_config.feedForwardMultiplier = 610; //gives 127 pwm at 0.4 commandedspeed  ORIG:320
     vel_config.integralDeadZone = 0.01; //set the integral dead zone, prevented integral from growing or s$
     vel_config.integralErrorHistoryLength = 10000; //how many time ticks should error history should be st$
     vel_config.integralMax = vel_config.satUpper/2; //what is the limit of the integral output for the PID
     vel_config.derivativeAlpha = 0.7; //dead code not used
 
     PIDConfig yaw_config;
-    yaw_config.Kp = 100;
-    yaw_config.Ki = 0;
-    yaw_config.Kd = 0;
+    yaw_config.Kp = 60;
+    yaw_config.Ki = 15;
+    yaw_config.Kd = 5;
     yaw_config.satUpper = 255;
     yaw_config.satLower = -255;
     yaw_config.antiWindup = yaw_config.satUpper/6;
