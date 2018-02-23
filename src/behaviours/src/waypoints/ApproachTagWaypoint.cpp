7d0edeb6d8a2a264fa37a0d0f0b96e81542a6787
Daniel R. Koris <korisd@gmail.com>
2018-02-23 18:06:38 -0500

    made these more configurable

--- src/behaviours/src/waypoints/ApproachTagWaypoint.cpp
+++ src/behaviours/src/waypoints/ApproachTagWaypoint.cpp
@@ -6,26 +6,26 @@ void ApproachTagWaypoint::run()
 
     if( this->has_arrived == false )
     {
-        if( TagUtilities::hasTag( &inputs->tags, this->desired_tag_id ) )
+        if( TagUtilities::hasTag( &inputs->tags, this->t_params.desired_tag) )
         {
             Tag current_tag = inputs->tags.back();
             double current_distance = TagUtilities::getDistance( current_tag );
 
-            if( current_distance <= this->desired_distance )
+            if( current_distance <= this->t_params.dist_goal )
                 has_arrived = true;
             else
             {
                 MotorParams m_params;
 
-                m_params.dist_deccel_point = 0.05;
+                m_params.dist_deccel_point = this->t_params.dist_deccel;
                 m_params.dist_current = current_distance;
-                m_params.dist_goal = this->desired_distance;
-                m_params.dist_max_output = 10;
+                m_params.dist_goal = this->t_params.dist_goal;
+                m_params.dist_max_output = this->t_params.dist_max_output;
 
-                m_params.yaw_deccel_point = .1;
+                m_params.yaw_deccel_point = this->t_params.yaw_deccel;
                 m_params.yaw_current = current_tag.getPositionX();
-                m_params.yaw_goal = 0.0;
-                m_params.yaw_max_output = 10;
+                m_params.yaw_goal = this->t_params.yaw_goal;
+                m_params.yaw_max_output = this->t_params.yaw_max_output;
 
                 leftAndRight = inputs->controller.generateSkidOutput( m_params );
 
