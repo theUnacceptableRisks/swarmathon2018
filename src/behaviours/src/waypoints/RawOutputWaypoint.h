39046d7add684d55dd5e1e315fa521a26a52f7c9
Daniel R. Koris <korisd@gmail.com>
2018-02-23 22:00:47 -0500

    started raw output waypoint

---
+++ src/behaviours/src/waypoints/RawOutputWaypoint.h
@@ -0,0 +1,13 @@
+#ifndef rawoutputwaypoint_h
+#define rawoutputwaypoint_h
+
+typedef struct rawoutputparams
+{
+    int left_output;
+    int right_output;
+    double duration;
+} RawOutParams;
+
+class RawOutputWaypoint :
+
+#endif
