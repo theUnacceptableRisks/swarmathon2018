ac5b46492d8db0dc92e364dc94c35c2238c0b7d2
Daniel R. Koris <korisd@gmail.com>
2018-03-07 13:06:39 -0500

    got basics in for dropoff state in

---
+++ src/behaviours/src/logic/DropOffState.cpp
@@ -0,0 +1,124 @@
+#include "PickUpState.h"
+#include "../Gripper.h"
+
+void DropOffState::action()
+{
+    forceTransition( internalTransition() );
+    internalAction();
+}
+
+void DropOffState::onEnter( std::string prev_state )
+{
+    if( prev_state == "findhome_state" )
+    {
+        forceTransition( DROPOFF_INIT );
+    }
+}
+
+void DropOffState::onExit( std::string nexst_state )
+{
+
+}
+
+std::string DropOffState::transition()
+{
+    std::string transition_to = getIdentifier();
+
+    switch( internal_state )
+    {
+        default:break;
+        case DROPOFF_COMPLETE:
+            transition_to = "search_state";
+    }
+
+    return transition_to;
+}
+
+DOState DropOffState::internalTransition()
+{
+    DOState transition_to = internal_state;
+
+    switch( internal_state )
+    {
+        default:break;
+        case DROPOFF_INIT:
+            if( this->approach )
+                transition_to = DROPOFF_APPROACH;
+            break;
+        case DROPOFF_APPROACH:
+            if( this->approach && this->approach->hasArrived() )
+            {
+                delete this->approach;
+                this->approach = 0;
+                this->outputs->current_waypoint = 0;
+
+                transition_to = DROPOFF_ADJUST;
+            }
+            break;
+    }
+
+    return transition_to;
+}
+
+void DropOffState::internalAction()
+{
+    switch( internal_state )
+    {
+        default:break;
+        case DROPOFF_INIT:
+        {
+            TagParams t_params;
+
+            t_params.desired_tag = 256;
+
+            t_params.dist_deccel = 0.05;
+            t_params.dist_goal = 0.24;
+            t_params.dist_max_output = 10;
+
+            t_params.yaw_deccel = 0.10;
+            t_params.yaw_goal = 0.0;
+            t_params.yaw_max_output = (80/3)
+
+            t_params.type = CLOSEST;
+
+            this->approach = new ApproachTagWaypoint( this->inputs, params );
+            this->outputs->current_waypoint = this->approach;
+            break;
+        }
+
+        case DROPOFF_APPROACH:
+            break;
+        case DROPOFF_ADJUST:
+            std::cout << "Made it to adjust" << std::endl;
+            break;
+    }
+}
+
+void DropOffState::forceTransition( DOState transition_to )
+{
+    DOState prev_state = internal_state;
+
+    internal_state = transition_to;
+
+    if( internal_state != prev_state )
+    {
+        /* on Exit */
+        switch( prev_state )
+        {
+            default:break;
+        }
+
+        /* on Enter */
+        switch( internal_state )
+        {
+            default: break;
+            case DROPOFF_INIT:
+                if( this->approach )
+                {
+                    delete this->approach;
+                    this->approach = 0;
+                }
+                break;
+        }
+    }
+}
--- src/behaviours/src/logic/DropOffState.h
+++ src/behaviours/src/logic/DropOffState.h
@@ -10,6 +10,7 @@
 typedef enum
 {
     DROPOFF_INIT,
+    DROPOFF_APPROACH,
     DROPOFF_ADJUST,
     DROPOFF_WIGGLE_LEFT,
     DROPOFF_WIGGLE_RIGHT,
@@ -23,7 +24,26 @@ class DropOffState : public State
 {
     public:
         DropOffState( IOTable *io ) : State( "dropoff_state" ), internal_state(DROPOFF_INIT), inputs(io->inputs), outputs(io->outputs) {}
+        virtual void action( void );
+        virtual void onEnter( std::string prev_state );
+        virtual void onExit( std::string next_state );
+        virtual std::string transition();
+
     private:
+        DOState internalTransition();
+        void internalAction();
+        void forceTransition( DOState transition_to );
+
+        ApproachTagWaypoint *approach;
+        RawOutputWaypoint *alignment;
+        RawOutputWaypoint *wiggle_left;
+        RawOutputWaypoint *wiggle_right;
+        LinearWaypoint *enter;
+
+        LogicInputs *inputs;
+        LogicOutputs *outputs;
+        DOState internal_state;
+
 };
 
 #endif
