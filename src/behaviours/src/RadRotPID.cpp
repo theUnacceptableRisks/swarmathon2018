4b595a67035083c2592109cbf459f273db3c0306
Daniel R. Koris <korisd@gmail.com>
2018-03-16 13:09:56 -0400

    wrote a radian rotation

---
+++ src/behaviours/src/RadRotPID.cpp
@@ -0,0 +1,27 @@
+#include "RadRotPID.h"
+
+std::tuple<int,int> RadRotPID::execute( PidInputs inputs )
+{
+    int output;
+
+    if( prev_goal != inputs.goal )
+        reset();
+
+    double error = angles::shortest_angular_distance( inputs.measured, inputs.goal );
+    double dt = inputs.time - prev_time;
+
+    output = runPID( fabs( error ), dt );
+
+    prev_time = inputs.time;
+    prev_goal = inputs.goal;
+
+    if( output > inputs.max_output )
+        output = inputs.max_output;
+
+    if( error >= 0 )
+    {
+        return std::make_tuple( output*(-1), output );
+    }
+
+    return std::make_tuple( output, output*(-1) );
+}
